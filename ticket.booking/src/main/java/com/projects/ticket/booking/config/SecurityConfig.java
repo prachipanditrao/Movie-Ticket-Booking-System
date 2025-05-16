7384b30f8fd12f422e44a2bbdc8d8304b4d8cf34
prachipanditrao <prachi.panditrao@gmail.com>
2025-05-16 17:23:03 +0530

    Disabled Cors

--- ticket.booking/src/main/java/com/projects/ticket/booking/config/SecurityConfig.java
+++ ticket.booking/src/main/java/com/projects/ticket/booking/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
+            .cors(ServerHttpSecurity.CorsSpec::disable)
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
             .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
             .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
